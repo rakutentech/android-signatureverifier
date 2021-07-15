8f54643a7855bbdbe47496c818f27f01e56118b3
Vincent Del Mundo <60253966+vincentdelmundo@users.noreply.github.com>
2021-07-15 11:14:35 +0900

    test: use test dispatcher for test coroutine (#14)

--- signatureverifier/src/test/kotlin/io/github/rakutentech/signatureverifier/integration/IntegrationSpec.kt
+++ signatureverifier/src/test/kotlin/io/github/rakutentech/signatureverifier/integration/IntegrationSpec.kt
@@ -1,14 +1,16 @@
 package io.github.rakutentech.signatureverifier.integration
 
 import androidx.test.core.app.ApplicationProvider
-import com.ibm.icu.impl.Assert
 import io.github.rakutentech.signatureverifier.IntegrationTests
+import io.github.rakutentech.signatureverifier.RealSignatureVerifier
 import io.github.rakutentech.signatureverifier.RobolectricBaseSpec
-import io.github.rakutentech.signatureverifier.SignatureVerifier
+import io.github.rakutentech.signatureverifier.api.ApiClient
+import io.github.rakutentech.signatureverifier.api.PublicKeyFetcher
+import io.github.rakutentech.signatureverifier.verification.PublicKeyCache
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.runBlockingTest
 import org.amshove.kluent.shouldBeTrue
-import org.amshove.kluent.shouldNotBeNull
 import org.junit.Test
 import org.junit.experimental.categories.Category
 
@@ -18,19 +20,24 @@ class IntegrationSpec : RobolectricBaseSpec() {
     @ExperimentalCoroutinesApi
     @Test
     fun `should return true for test data`() = runBlockingTest {
-        val instance = SignatureVerifier.init(
-            ApplicationProvider.getApplicationContext(),
-            System.getenv("IT_ENDPOINT") ?: "",
-            System.getenv("IT_SUB_KEY") ?: ""
-        ) {
-            Assert.fail(it)
-        }
+        val baseUrl = System.getenv("IT_ENDPOINT") ?: ""
+        val client = ApiClient(
+            baseUrl = baseUrl,
+            subscriptionKey = System.getenv("IT_SUB_KEY") ?: "",
+            context = ApplicationProvider.getApplicationContext()
+        )
 
         val keyId = System.getenv("IT_KEY_ID") ?: ""
         val data = System.getenv("IT_DATA") ?: ""
         val signature = System.getenv("IT_SIGNATURE") ?: ""
 
-        instance.shouldNotBeNull()
+        val instance = RealSignatureVerifier(
+            PublicKeyCache(
+                keyFetcher = PublicKeyFetcher(client),
+                context = ApplicationProvider.getApplicationContext(),
+                baseUrl = baseUrl
+            ), TestCoroutineDispatcher()
+        )
         instance.verify(keyId, data.byteInputStream(), signature).shouldBeTrue()
     }
 }
